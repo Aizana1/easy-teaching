903bb349be98a1a919ec473c4d28046cf614cba5
Aizana <ayzana2010@gmail.com>
2021-07-23 18:59:06 +0300

    video

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,21 @@
+import Video from './components/Video'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MainPage from './components/MainPage'
+import Notification from './components/Notifications'
+import Editor from './socket/Editor'
+
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+   return (
+    <Router>
+      <Route path="/lessons/:id">
+        <MainPage>
+          <Notification />
+          <Video />
+          <Editor />
+          </MainPage>
+      </Route>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {ContextProvider} from './socket/SocketContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ContextProvider>
+        <App />
+  </ContextProvider>,
   document.getElementById('root')
 );
 
