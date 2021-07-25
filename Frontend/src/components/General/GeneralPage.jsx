c3cb82c812fcb581e0fb2f29df4726ff6588abaa
Ruslan <ruslandegtyrev@gmail.com>
2021-07-25 19:46:20 +0300

    Поправил хеддер, сделал верное отображение от статуса юзера, убрал лишние компоненты и настроил пути

---
+++ Frontend/src/components/General/GeneralPage.jsx
@@ -0,0 +1,9 @@
+export const GeneralPage = () => {
+  return ( 
+    <div>
+      <h1>Общая страница</h1>
+    </div>
+   );
+}
+ 
+
---
+++ Frontend/src/components/student/main/MainStudent.jsx
@@ -0,0 +1,9 @@
+export const MainStudent = () => {
+  return ( 
+    <div>
+      <h1>Главная страница студента</h1>
+    </div>
+   );
+}
+ 
+
