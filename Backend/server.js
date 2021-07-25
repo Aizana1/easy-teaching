b1e89293e3fd32c46748edb9392ec87a8ee5e9c1
Aizana <ayzana2010@gmail.com>
2021-07-25 12:53:53 +0300

    fixing my pull

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,24 @@ const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const connect = require('./db/connect');
+const http = require('http')
 const cors = require("cors");
 const app = express();
-
-app.use(cors({origin: true, credentials: true,}));
-
+const server = http.createServer(app)
+// app.use(cors({origin: true, credentials: true,}));
+const Document = require("./Document")
 
 connect.connect();
+
+const PORT = process.env.PORT | 5500
+const io = require('socket.io')(server, {
+  allowEIO3: true,
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+})
 
 // Подключаем middleware morgan с режимом логирования "dev", чтобы для каждого HTTP-запроса на сервер в консоль выводилась информация об этом запросе.
 app.use(logger('dev'));
@@ -34,7 +45,40 @@ app.use(session(sessionConfig));
 //   next();
 // });
 
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
 
-app.listen(process.env.PORT, () => {
-  console.log(`server started PORT: ${process.env.PORT}`);
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocument(documentId)
+    socket.join(documentId)
+    socket.emit("load-document", document.data)
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta)
+    })
+
+    socket.on("save-document", async data => {
+      await Document.findByIdAndUpdate(documentId, { data })
+    })
+  })
+});
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Error: ${err}`)
+    return
+  }
+  console.log(`Server started on port: ${PORT}`)
 })
